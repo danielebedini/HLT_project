ce465b544568ff73cf5cb3d24b120477493a80f2
Daniele Bedini <108469620+danielebedini@users.noreply.github.com>
2024-04-12 10:59:12 +0200

    Added balanced data

--- data.py
+++ data.py
@@ -3,27 +3,36 @@ from sklearn.model_selection import train_test_split
 from utils import preprocess_text, preprocess_text_porter_stemmer, preprocess_text_v2, preprocess_text_contractions
 
 # Carica il dataset
-data = pd.read_csv('amazon_reviews.csv')
-
+#data = pd.read_csv('amazon_reviews.csv')
+data_balanced = pd.read_csv('new_balanced_data.csv')
 # Visualizza le prime righe del dataset
-print(data.head())
+print(data_balanced.head())
 
 # apply preprocessing to the column named 'reviewText'
-data['CleanedText'] = data['reviewText'].apply(preprocess_text)
+data_balanced['CleanedText'] = data_balanced['reviewText'].apply(preprocess_text)
 #data['CleanedText'] = data['reviewText'].apply(preprocess_text_porter_stemmer)
 #data['CleanedText'] = data['reviewText'].apply(preprocess_text_v2)
 #data['CleanedText'] = data['reviewText'].apply(preprocess_text_contractions)
 
 
 # remove rows with missing values in the 'CleanedText' column
-data = data.dropna(subset=['CleanedText'])
+data_balanced = data_balanced.dropna(subset=['CleanedText'])
 
 # visualize only a few columns
-print(data[['CleanedText', 'overall']].head())
+print(data_balanced[['CleanedText', 'overall']].head())
 
 # divide the dataset into features and target
-X = data['CleanedText']
-y = data['overall']
+X = data_balanced['CleanedText']
+y = data_balanced['overall']
 
 # divide the dataset into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#X_train_balanced = data_balanced['CleanedText']
+#y_train_balanced = data_balanced['overall']
+
+# divide the balanced dataset into training and test sets
+
+X_train_balanced, X_test_balanced, y_train_balanced, y_test_balanced = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# print the shape of the training and test sets
