70d1de44580d1721e819d4165b26794c545af803
Daniele Bedini <108469620+danielebedini@users.noreply.github.com>
2024-04-12 10:59:56 +0200

    Fixed model with balanced data

--- model_lsvc.py
+++ model_lsvc.py
@@ -3,7 +3,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score, classification_report
 
-from data import data, X_train, X_test, y_train, y_test
+from data import X_train_balanced, X_test_balanced, y_train_balanced, y_test_balanced
 
 # here, we can tune the hyperparameters of the TfidfVectorizer
 tfidf = TfidfVectorizer(max_features=10000, ngram_range=(1, 2), min_df=5, max_df=0.8)
@@ -15,12 +15,12 @@ model_lsvc = Pipeline([
 ])
 
 # train the model on the training set
-model_lsvc.fit(X_train, y_train)
+model_lsvc.fit(X_train_balanced, y_train_balanced)
 
 # evaluate the model on the test set
-y_pred = model_lsvc.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
+y_pred = model_lsvc.predict(X_test_balanced)
+accuracy = accuracy_score(y_test_balanced, y_pred)
 print(f'Accuracy: {accuracy}')
 
 # visualize the classification report
-print(classification_report(y_test, y_pred))
+print(classification_report(y_test_balanced, y_pred))
