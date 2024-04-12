b348959a7ede8bca96586e1d53d864a27a80cc53
Daniele Bedini <108469620+danielebedini@users.noreply.github.com>
2024-04-12 11:00:16 +0200

    Fixed model with balanced data

--- model_rfc.py
+++ model_rfc.py
@@ -3,7 +3,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-from data import X_train, X_test, y_train, y_test
+from data import X_train_balanced, X_test_balanced, y_train_balanced, y_test_balanced
 
 # create the pipeline for the RandomForestClassifier
 model_rfc = Pipeline([
@@ -12,9 +12,9 @@ model_rfc = Pipeline([
 ])
 
 # training the model on the training set
-model_rfc.fit(X_train, y_train)
+model_rfc.fit(X_train_balanced, y_train_balanced)
 
 # evaluation of the model on the test set
-y_pred_rf = model_rfc.predict(X_test)
-accuracy_rf = accuracy_score(y_test, y_pred_rf)
+y_pred_rf = model_rfc.predict(X_test_balanced)
+accuracy_rf = accuracy_score(y_test_balanced, y_pred_rf)
 print(f'Accuracy with RandomForestClassifier: {accuracy_rf}')
