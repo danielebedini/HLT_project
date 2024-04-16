69e1682e0a2ea39e22b1908b492502e58710b397
Daniele Bedini <108469620+danielebedini@users.noreply.github.com>
2024-04-16 17:42:47 +0200

    Fix

--- model_LLM.py
+++ model_LLM.py
@@ -1,12 +1,9 @@
 # Large language model for sentiment analysis
-import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, classification_report
-from sklearn.model_selection import train_test_split
 
-from utils import preprocess_text_v2
 from data import X_train_balanced, X_test_balanced, y_train_balanced, y_test_balanced
 
 # create a pipeline
