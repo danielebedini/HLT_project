54e478d5f51c7d4c52e5390025a92cbb8a61ee85
Daniele Bedini <108469620+danielebedini@users.noreply.github.com>
2024-06-22 09:44:06 +0200

    Fix

--- model_lsvc.py
+++ model_lsvc.py
@@ -17,7 +17,7 @@ class LSVCModelBuilder:
     def train(self, X_train, y_train):
         self.model.fit(X_train, y_train)
 
-    def evaluate(self, X_test, y_test): # It can take even validation data
+    def evaluate(self, X_test, y_test):
         y_pred = self.model.predict(X_test)
         accuracy = accuracy_score(y_test, y_pred)
         f1 = f1_score(y_test, y_pred, average='weighted')
